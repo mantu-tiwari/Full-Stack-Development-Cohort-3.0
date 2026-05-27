e8a56a91ad3e9dc3977acbf3923bc078da6ec2ed
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-05-27 23:29:56 +0530

    loop in js

---
+++ Frontend/03 JS/03 Phase 2 JS/script.js
@@ -0,0 +1,64 @@
+console.log('hello');
+// While loop
+
+// var a = 5
+// while (a<10) {
+//     console.log(a);
+//     a++
+// }
+
+// var a = 2
+// while(a<=10){
+//     console.log(a);
+//     a++
+// }
+
+// var a = 5 
+// while (a<=50) {
+//     console.log(a);
+//     a +=2 
+// }
+
+// var a = 0
+// while (a<10) {
+//     console.log(a);
+//     a++
+// }
+
+// // Ask a Number to user and print is table
+// var num = Number(prompt('Enter the Number: '))
+// var i = 1
+// while (i<=10) {
+//     console.log(`${num} X ${i} = ${num*i} `);
+//     i++
+// }
+
+// // password checker (bug in advance version)
+// var count = 0
+// do{
+//     var pass = Number(prompt('Enter the Number'))
+//     console.log('Invalid');
+//     count ++
+//     if (count === 3){
+//         console.log(alert('Warning'));
+//         break
+//     }else{
+//         console.log('connected');
+//     }
+// }while(pass != '12345')
+//     console.log("unsuccessfull");
+//     // console.log("Connected");
+
+// For loop (used by 90% developer)
+
+// const a = 10
+// a = 20
+// console.log(a);
+// a++
+// console.log(a);
+
+if (2 > 1){
+    const b = 10;
+    console.log(b);
+}
+console.log(b);
