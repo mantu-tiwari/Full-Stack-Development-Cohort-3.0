a6dd3a1c98ba48029db55ad1b5f6ae151446647a
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-04 17:32:22 +0530

    js phase 3

--- Frontend/03 JS/08 Phase 3 JS working/script.js
+++ Frontend/03 JS/08 Phase 3 JS working/script.js
@@ -1,1 +1,10 @@
-console.log('testing');
+// console.log('testing');
+function first() {
+    console.log('first');
+    function second() {
+        console.log('second');    
+    }
+    second()
+    return 20
+}
+console.log(first());
