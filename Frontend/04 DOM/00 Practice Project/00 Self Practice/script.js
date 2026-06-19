c67a8fb3cbd10214e291914da4e65bb07030e8b3
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-19 23:06:03 +0530

    practice self

---
+++ Frontend/04 DOM/00 Practice Project/00 Self Practice/script.js
@@ -0,0 +1,8 @@
+// Select the <h1> and print it in console.
+const h1 = document.querySelector('h1')
+console.log(h1);
+// Select all paragraphs and print them.
+const p = document.querySelectorAll('p')
+console.log(p);
+const p1 = document.getElementsByTagName('p')
+console.log(p1);b
