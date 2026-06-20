01315595f118ba8d417b86d71daa57bfb24112d3
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-20 19:23:14 +0530

    random color added

--- Frontend/04 DOM/05 DOM Event Delegation/script.js
+++ Frontend/04 DOM/05 DOM Event Delegation/script.js
@@ -6,26 +6,39 @@
 //   console.log(sum);
 // });
 
-// ********************************* Project 
+// ********************************* Project
 // Start button pe click karo ye apna position change karega
 const main = document.querySelector("main");
-const box = document.querySelector(".box");
 const btn = document.querySelector("button");
-const p = document.querySelector('p')
+const p = document.querySelector("p");
+const box = document.createElement('div')
+box.classList.add('box')
+let time = 0;
+let move;
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return (`rgb(${r},${g},${b})`);
+}
+const randomBox = () => {
+  main.append(box)
+  p.innerHTML = `<p>Time : ${time++} sec </p>`;
+  const top = Math.floor(Math.random() * 90);
+  const left = Math.floor(Math.random() * 90);
+  box.style.left = left + "%";
+  box.style.top = top + "%";
+};
 btn.addEventListener("click", () => {
-  let time = 1
-  const move = setInterval(() => {
-    p.innerHTML = `<p>Time : ${time++} sec </p>`
-    const top = Math.floor(Math.random() * 90);
-    const left = Math.floor(Math.random() * 90);
-    box.style.left = left+'%'
-    box.style.top = top+'%'
+  randomBox()
+  move = setInterval(() => {
+    box.style.backgroundColor = randomColor()
+    randomBox();
   }, 1000);
   setTimeout(() => {
-    clearInterval(move)
-    console.log('stopped');
+    clearInterval(move);
+    console.log("stopped");
   }, 10000);
 });
-
 
 
