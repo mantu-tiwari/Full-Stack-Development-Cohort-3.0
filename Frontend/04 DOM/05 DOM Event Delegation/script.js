cb9e977027127cfa95177345618519a01510183f
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-20 17:59:55 +0530

    project stage 1 done

--- Frontend/04 DOM/05 DOM Event Delegation/script.js
+++ Frontend/04 DOM/05 DOM Event Delegation/script.js
@@ -1,7 +1,20 @@
-// Event Delegation
+// // Event Delegation
+// const main = document.querySelector("main");
+// let sum = 0;
+// main.addEventListener("click", (e) => {
+//   sum += Number(e.target.textContent);
+//   console.log(sum);
+// });
+
+// ********************************* Project 
+// Start button pe click karo ye apna position change karega
 const main = document.querySelector("main");
-let sum = 0;
-main.addEventListener("click", (e) => {
-  sum += Number(e.target.textContent);
-  console.log(sum);
+const box = document.querySelector(".box");
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const top = Math.floor(Math.random() * 90);
+  const left = Math.floor(Math.random() * 90);
+  box.style.left = left+'%'
+  box.style.top = top+'%'
 });
+
