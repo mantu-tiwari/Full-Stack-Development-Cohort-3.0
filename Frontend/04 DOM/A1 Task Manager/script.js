5b3d79a2ddc761bcc498b6dd88afff6355e6d2c6
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-26 00:01:27 +0530

    create task initiated

--- Frontend/04 DOM/A1 Task Manager/script.js
+++ Frontend/04 DOM/A1 Task Manager/script.js
@@ -1,1 +1,7 @@
-console.log('testing');
+const createBtn = document.querySelector('.create-btn')
+const formClass = document.querySelector('.form')
+
+createBtn.addEventListener('click',() => {
+    formClass.style.display = 'flex'
+})
+
