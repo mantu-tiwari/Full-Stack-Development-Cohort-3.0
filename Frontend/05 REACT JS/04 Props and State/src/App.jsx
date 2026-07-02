a312c355571f015809c69a13473d99d5c8f44823
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-02 06:46:30 +0530

    import export concept

---
+++ Frontend/05 REACT JS/04 Props and State/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import noName, {one, two} from './test'
+
+const App = () => {
+
+  noName()
+  one()
+  two()
+  return (
+    <div>
+      <h1>Boiler Plate of React is rafce</h1>
+    </div>
+  )
+}
+
+export default App
+
---
+++ Frontend/05 REACT JS/04 Props and State/src/test.jsx
@@ -0,0 +1,14 @@
+export const one = () => {
+    console.log('one');
+}
+export const two = () => {
+    console.log('two');
+}
+const three = () => {
+    console.log('three');
+}
+const four = () => {
+    console.log('four');
+}
+
+export default three
