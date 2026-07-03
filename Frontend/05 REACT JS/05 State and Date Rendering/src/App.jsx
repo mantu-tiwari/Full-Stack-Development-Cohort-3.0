c45be27905e99622bbc56fe521cb76d29d6e539c
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-03 08:04:10 +0530

    styling

---
+++ Frontend/05 REACT JS/05 State and Date Rendering/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-xl'>hello</h1>
+      <h3 className='text-xl background red'>Testing</h3>
+    </div>
+  )
+}
+
+export default App
