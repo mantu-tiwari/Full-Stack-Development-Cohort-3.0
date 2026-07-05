9cdf8c9a6a3a897dd72925f7517bf80018a0b56f
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-05 07:29:40 +0530

    add transaction funcanality

--- Frontend/04 DOM/A2 FinTrack Pro/script.js
+++ Frontend/04 DOM/A2 FinTrack Pro/script.js
@@ -3,9 +3,16 @@ const addBtn = document.querySelector('.transaction-btn')
 const form = document.querySelector('.form-section')
 const formBg = document.querySelector('.form-bg')
 const close = document.querySelector('.form-head i')
+const transactionCont = document.querySelector('.detail-container')
+const formContainer = document.querySelector('form')
+const deskInp = document.querySelector('#desk-inp')
+const amountInp = document.querySelector('#amount-inp')
+const dateInp = document.querySelector('#date-inp')
+const categoryInp = document.querySelector('#category-inp')
+const container = document.querySelector('.detail-container')
 
 
-// Add Transaction Form open and close
+// (Add Transaction Form) open and close
 const openForm = () => {
     formBg.style.display = 'flex'
     form.style.display = 'flex'
@@ -19,4 +26,30 @@ close.addEventListener('click',closeForm)
 formBg.addEventListener('click',closeForm)
 form.addEventListener('click',(e) => {
     e.stopPropagation()
+})
+
+// Add Transaction to All Transaction section
+formContainer.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let description = deskInp.value
+    let amount = amountInp.value
+    let date = dateInp.value
+    let category = categoryInp.value
+    if(description.trim() === '' || amount.trim() === '' || date.trim() === '' || category.trim() === ''){
+        alert('Kindly fill all details')
+        return
+    }
+    container.innerHTML += `<div class="detail-card">
+                <h4 id="tarik">${date}</h4>
+                <h4>${description}</h4>
+                <button>${category}</button>
+                <h4>${amount}</h4>
+                <div class="act-btn">
+                    <i class="fa-solid fa-pen"></i>
+                    <i class="fa-solid fa-trash"></i>
+                </div>
+            </div>`
+
+    closeForm()
+    formContainer.reset()
 })
