e298f2cb2679973c210a56ac9c58d9ec2621c39b
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-14 00:23:36 +0530

    optimised form handling

--- Frontend/05 REACT JS/06 Rendering and Form Handling/src/Form.jsx
+++ Frontend/05 REACT JS/06 Rendering and Form Handling/src/Form.jsx
@@ -43,32 +43,73 @@
 // };
 // export default Form;
 
-// Good Approach 
-import React, { useState } from 'react'
+// // Good Approach
+// import React, { useState } from 'react'
+// const Form = () => {
+//     const [data, setData] = useState({
+//         // name: '',
+//         // email: '',
+//         // password: ''
+//     })
+//     console.log('form data',data);
+//   return (
+//     <div>
+//         <input onChange={(e) => {
+//             setData({...data, name:e.target.value})
+//         }} type="text" placeholder='name' />
+//         <input onChange={(e) => {
+//             setData({...data, email: e.target.value})
+//         }} type="text" placeholder='email' />
+//         <input onChange={(e) => {
+//             setData({...data, password:e.target.value})
+//         }} type="text" placeholder='password' />
+//         <button>Submit</button>
+//       <h1>This is name: {} </h1>
+//       <h1>This is email: {} </h1>
+//       <h1>This is password: {} </h1>
+//     </div>
+//   )
+// }
+// export default Form
+
+// Optamised Approach
+import React, { useState } from "react";
 const Form = () => {
-    const [data, setData] = useState({
-        // name: '',
-        // email: '',
-        // password: ''
-    })
-    console.log('form data',data);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  console.log(data);
+  const handleChange = (e) => {
+    let {name, value} = e.target // doubt in this line
+    setData({ ...data, [name]: value });
+  };
   return (
     <div>
-        <input onChange={(e) => {
-            setData({...data, name:e.target.value})
-        }} type="text" placeholder='name' />
-        <input onChange={(e) => {
-            setData({...data, email: e.target.value})
-        }} type="text" placeholder='email' />
-        <input onChange={(e) => {
-            setData({...data, password:e.target.value})
-        }} type="text" placeholder='password' />
-        <button>Submit</button>
-      <h1>This is name: {} </h1>
-      <h1>This is email: {} </h1>
-      <h1>This is password: {} </h1>
+      <input
+        name="name"
+        onChange={handleChange}
+        type="text"
+        placeholder="name"
+      />
+      <input
+        name="email"
+        onChange={handleChange}
+        type="text"
+        placeholder="email"
+      />
+      <input
+        name="password"
+        onChange={handleChange}
+        type="text"
+        placeholder="password"
+      />
+      <button>Submit</button>
+      <h1>name: {data.name} </h1>
+      <h1>email: {data.email} </h1>
+      <h1>password: {data.password} </h1>
     </div>
-  )
-}
-export default Form
-
+  );
+};
+export default Form;
