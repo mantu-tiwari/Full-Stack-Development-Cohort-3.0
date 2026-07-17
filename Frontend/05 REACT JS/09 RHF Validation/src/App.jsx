af0fb3b941283e0df7ea3066f0bb74121c9c74a1
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-17 11:53:32 +0530

    navbar done

--- Frontend/05 REACT JS/09 RHF Validation/src/App.jsx
+++ Frontend/05 REACT JS/09 RHF Validation/src/App.jsx
@@ -1,26 +1,40 @@
-// Revision 
-import React, { useState } from 'react'
-import Register from './revision/Register'
-import Login from './revision/Login'
-import Card from './revision/Card'
-import LoginUseref from './revision/LoginUseref'
-import RHF from './revision/RHF'
+// // Revision 
+// import React, { useState } from 'react'
+// import Register from './revision/Register'
+// import Login from './revision/Login'
+// import Card from './revision/Card'
+// import LoginUseref from './revision/LoginUseref'
+// import RHF from './revision/RHF'
+// const App = () => {
+//   const [toggle, setToggle] = useState(false)
+//   const [arr, setArr] = useState([])
+//   console.log(arr);
+//   return (
+//     <div>
+//         <h1>You are in App component</h1>
+//         {toggle ? <Register setToggle={setToggle} setArr={setArr} /> : <LoginUseref setToggle={setToggle} />}
+//         <h1 >Welcome to Card</h1>
+//         <div className='flex gap-4 flex-wrap min-h-40' >
+//           {arr.map((e) => (
+//               <Card user={e}/>
+//           ))}
+//         </div>
+//         <RHF/>
+//     </div>
+//   )
+// }
+// export default App
+
+import React from 'react'
+import Navbar from './components/Navbar'
+
 const App = () => {
-  const [toggle, setToggle] = useState(false)
-  const [arr, setArr] = useState([])
-  console.log(arr);
   return (
-    <div>
-        <h1>You are in App component</h1>
-        {toggle ? <Register setToggle={setToggle} setArr={setArr} /> : <LoginUseref setToggle={setToggle} />}
-        <h1 >Welcome to Card</h1>
-        <div className='flex gap-4 flex-wrap min-h-40' >
-          {arr.map((e) => (
-              <Card user={e}/>
-          ))}
-        </div>
-        <RHF/>
+    <div className='p-4 '>
+      <Navbar/>
     </div>
   )
 }
+
 export default App
+
---
+++ Frontend/05 REACT JS/09 RHF Validation/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between bg-amber-300 rounded-lg p-4'>
+      <div className="logo ">
+        <img className='w-[50px]' src="https://img.icons8.com/?size=100&id=108296&format=png&color=000000" alt="user logo" />
+      </div>
+      <div className="links flex items-center gap-4 font-bold cursor-pointer">
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+      <button className='border rounded-4xl cursor-pointer bg-blue-600 text-white font-bold px-8'>Create User</button>
+    </div>
+  )
+}
+
+export default Navbar
