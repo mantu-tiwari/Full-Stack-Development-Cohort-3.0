335d7c7267a34bd4313ae764cfc245d0574a345e
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-22 23:45:29 +0530

    reviosion done

--- Frontend/05 REACT JS/10 LS Context API/src/revision/RHF.jsx
+++ Frontend/05 REACT JS/10 LS Context API/src/revision/RHF.jsx
@@ -1,29 +1,62 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-const RHF = ({setArr}) => {
-  
+const RHF = ({ setArr }) => {
   console.log("RHF Rendering");
-  const {handleSubmit, register, reset, formState:{errors}} = useForm();
-  console.log('here is the error', errors);
-const submitData = (data) => {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange'
+  });
+  console.log("here is the error", errors);
+  const submitData = (data) => {
     console.log(data);
-    setArr(prev => [...prev, data])
-    reset()
-}
+    setArr((prev) => [...prev, data]);
+    reset();
+  };
 
   return (
     <div>
       <h1>RHF form (with validation) </h1>
-      <form onSubmit={handleSubmit(submitData)} className="w-fit border rounded-lg flex flex-col">
-        <input {...register('email',{
-            required: 'email is required',
-        })} type="text" placeholder="email" />
-        <p className="text-red-500">Email do </p>
-        <input {...register('password', {
-            required: 'password is required'
-        })} type="text" placeholder="password" />
-        <p className="text-red-500">password do</p>
+      <form
+        onSubmit={handleSubmit(submitData)}
+        className="w-fit border rounded-lg flex flex-col"
+      >
+        <input
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: 'Invlid email address'
+            }
+          })}
+          type="text"
+          placeholder="email"
+        />
+        {errors.email && (
+          <p className="text-red-500"> {errors.email.message} </p>
+        )}
+        <input
+          {...register("password", {
+            required: "password is required",
+            minLength: {
+              value: 10,
+              message: "Minimum 10 Digit is Required",
+            },
+            maxLength: {
+              value: 10,
+              message: "Maximum 10 Digit is Required",
+            },
+          })}
+          type="text"
+          placeholder="password"
+        />
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
         <button>Submit</button>
       </form>
     </div>
