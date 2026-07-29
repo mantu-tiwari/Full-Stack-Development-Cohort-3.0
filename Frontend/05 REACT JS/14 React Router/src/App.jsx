90bfa7a05c0ec0587c7e84adb0413d9e54713c51
Mantu Kumar Tiwari <mantutiwari.ai@gmail.com>
2026-07-30 00:49:20 +0530

    before react router

--- Frontend/05 REACT JS/14 React Router/src/App.jsx
+++ Frontend/05 REACT JS/14 React Router/src/App.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
-  return (
-    <div>
-      <h1 >Initial Setup</h1>
-    </div>
-  )
-}
 
-export default App
+
+  const [toggle, setToggle] = useState('home')
+  
+  return (
+    
+    
+    <div>
+      <nav className="flex justify-between items-center p-4">
+        <div>
+          <h1>Logo</h1>
+        </div>
+        <div className="flex justify-between items-center gap-4">
+          <h1 onClick={() => {
+              setToggle('home')
+          }} className="border rounded cursor-pointer px-2">Home</h1>
+          <h1 onClick={() => {
+              setToggle('contact')
+          }} className="border rounded cursor-pointer px-2">Contact</h1>
+          <h1 onClick={() => {
+              setToggle('about')
+          }} className="border rounded cursor-pointer px-2">About</h1>
+        </div>
+        <button>Login</button>
+      </nav>
+
+      {/* These two method are bad practice react router bypass this  */}
+      {/* {toggle ? <Home/> : <Contact/>} */}
+      <div>
+        {toggle === 'home' && <Home/>}
+        {toggle === 'about' && <About/>}
+        {toggle === 'contact' && <Contact/>}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Frontend/05 REACT JS/14 React Router/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className='bg-green-500'>This is contct </h1>
+    </div>
+  )
+}
+
+export default Contact
