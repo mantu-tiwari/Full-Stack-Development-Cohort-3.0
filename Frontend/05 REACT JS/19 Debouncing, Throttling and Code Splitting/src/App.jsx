28a27c3948b8b233ecee47eec4baca0063102144
Mantu Kumar Tiwari <mantutiwari.ai@gmail.com>
2026-08-18 00:47:30 +0530

    debouncing

--- Frontend/05 REACT JS/19 Debouncing, Throttling and Code Splitting/src/App.jsx
+++ Frontend/05 REACT JS/19 Debouncing, Throttling and Code Splitting/src/App.jsx
@@ -1,40 +1,64 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-
-  const [productData, setProductData] = useState([])
-  const [searchData, setSearchData] = useState(null)
-  console.log(productData);
-  console.log(searchData);
+  const [productData, setProductData] = useState([]);
+  const [searchData, setSearchData] = useState(null);
+  // console.log(productData);
+  // console.log(searchData);
 
   const getProductData = async () => {
     try {
-      let res = await axios.get('https://fakestoreapi.com/products')
-      setProductData(res.data)
+      let res = await axios.get("https://fakestoreapi.com/products");
+      setProductData(res.data);
     } catch (error) {
-      console.log('the error is : ', error);
+      console.log("the error is : ", error);
     }
-  }
+  };
   useEffect(() => {
-      getProductData()
-  },[])
+    getProductData();
+  }, []);
+
+  let filteredData = () => {
+    console.log("filter data is running...");
+    let result = productData.filter((e) => {
+      return e.title.toLowerCase().includes(searchData.toLowerCase());
+    });
+    setProductData(result);
+  };
+
+  // deboouncing logic code
+  useEffect(() => {
+    if (!searchData) return;
+    let timeout = setTimeout(() => {
+      filteredData();
+    }, 700);
+    return () => clearTimeout(timeout) // ye tab chalega jab new aayega or purana jayega
+  }, [searchData]);
 
   return (
-    <div className='p-4'>
-      <input onChange={(e) => {
-          setSearchData(e.target.value)
-      }} className='border p-2 rounded-lg' type="text" placeholder='Search' />
-      <div className='p-4'>
-        {
-          productData.map((e) => {
-              return <h1 key={e.id} >{e.title} </h1>
-          })
-        }
+    <div className="p-4">
+      <input
+        onChange={(e) => {
+          setSearchData(e.target.value);
+        }}
+        className="border p-2 rounded-lg"
+        type="text"
+        placeholder="Search"
+      />
+      <div className="p-4">
+        {productData.map((e) => {
+          return (
+            <h1 key={e.id}>
+              {" "}
+              {e.id}. {e.title}{" "}
+            </h1>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
