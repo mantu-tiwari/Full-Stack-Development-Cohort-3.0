171c812823768f850b1a0abb9a675d189cb03a28
Mantu Kumar Tiwari <mantutiwari.ai@gmail.com>
2026-08-23 01:07:44 +0530

    counter logic

--- Frontend/05 REACT JS/20 React Redux Toolkit/src/App.jsx
+++ Frontend/05 REACT JS/20 React Redux Toolkit/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counterSlice";
 const App = () => {
+  const dispatch = useDispatch();
+  const { count } = useSelector((store) => store.counter);
+
   return (
     <div>
-      <h1>hello welocme to react redux toolkit</h1>
+      <h1>My count is : {count} </h1>
+      <button onClick={() => {
+          dispatch(decrement())
+      }} className="border px-2 ">decrease</button>
+      <button onClick={() => {
+          dispatch(increment())
+      }} className="border px-2 ">Increase</button>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
