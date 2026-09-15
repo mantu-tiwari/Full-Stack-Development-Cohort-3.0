f4e8e7bd077d50613824e57822b5ef800300a972
Mantu Kumar Tiwari <mantutiwari.ai@gmail.com>
2026-09-15 14:52:00 +0530

    typescript basics

--- Frontend/06 TYPESCRIPT/02 Typescript working with React/index.js
+++ Frontend/06 TYPESCRIPT/02 Typescript working with React/index.js
@@ -1,40 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 let a = 34;
 let b = "this is string";
 console.log(a);
 console.log(b);
-
 // Type inference and annotaion
 // inference - first time jo data pass karenge wo khud ko assign kar lega
 // annotation - isme data types ham provide karenge
-let x: string = "rahul";
+let x = "rahul";
 x = "sohan";
 console.log(x);
-
 // Primitive data types
 // string, boolean, bigint, number, symbol, undefined
-let name: string = "rahul";
+let name = "rahul";
 console.log(name);
-
 // Array and Tuples
 // array me kitna bhi data assign kar sakte hai jabki tuples me predefined karna parega ki kitna data jane wla hai
-
-let arr: any[] = [34, 4543, "hello", true, undefined];
+let arr = [34, 4543, "hello", true, undefined];
 console.log(arr);
-
-let aa: any[] = [23, "jio", true, undefined];
-let bb: number[] = [78, 32, 534, 234];
+let aa = [23, "jio", true, undefined];
+let bb = [78, 32, 534, 234];
 let cc = ["true", "console"];
-
 // Tuples
-let ttuple: [number, number] = [343, 5343];
-let data: any[] = [{ name: "rohan" }, { name: "kartik" }, { name: "sohail" }];
+let ttuple = [343, 5343];
+let data = [{ name: "rohan" }, { name: "kartik" }, { name: "sohail" }];
 console.log(data);
-
 // union types - ek se jyada types ke requirement ke liye use hota hai
-let yolo: number | string | boolean = "sohan";
+let yolo = "sohan";
 yolo = true;
 yolo = 342342;
-
-// literals - isme ham apna khud ka options assign kar sakte hai
-type Status = "Complete" | "Pending" | "Not Complete";
-let getStatus: Status = "Complete"; // sirf Status me se hi correct lega else error lega
+let getStatus = "Complete"; // sirf Status me se hi correct lega else error lega
+//# sourceMappingURL=index.js.map
